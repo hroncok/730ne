16c749233c65f215c486b1bb35cb4412a8021f6b
Miro Hrončok <miro@hroncok.cz>
2015-03-12 14:04:13 +0100

    Use PostgreSQL

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -96,8 +96,12 @@ TEMPLATE_DIRS = (
 if ON_OPENSHIFT:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['OPENSHIFT_APP_NAME'],
+            'USER': os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],
+            'PASSWORD': os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'],
+            'HOST': os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
+            'PORT': os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'],
         }
     }
 else:
