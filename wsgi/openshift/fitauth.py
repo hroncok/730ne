3f2d134c93f9580d156e12cef9d0e11d0b918879
Miro Hrončok <miro@hroncok.cz>
2015-03-12 23:35:29 +0100

    Retrieve more data from Usermap API

--- wsgi/openshift/fitauth.py
+++ wsgi/openshift/fitauth.py
@@ -8,16 +8,27 @@ class FITOAuth2(BaseOAuth2):
     ACCESS_TOKEN_URL = 'https://auth.fit.cvut.cz/oauth/oauth/token'
     ACCESS_TOKEN_METHOD = 'POST'
     ID_KEY = 'user_id'
+    EXTRA_DATA = [('roles', 'roles')]
 
     def get_user_details(self, response):
         """Return user details from FIT account"""
         return {'username': response.get('user_id'),
-                'email': response.get('user_email')}
+                'email': response.get('user_email'),
+                'first_name': response.get('firstName'),
+                'last_name': response.get('lastName')}
 
     def user_data(self, access_token, *args, **kwargs):
         """Loads user data from service"""
         url = 'https://auth.fit.cvut.cz/oauth/api/v1/tokeninfo'
         try:
-            return self.get_json(url, params={'token': access_token})
+            data = self.get_json(url, params={'token': access_token})
         except ValueError:
             return None
+        usermap_url = 'https://kosapi.fit.cvut.cz/usermap/v1/people/' + data['user_id']
+        try:
+            usermap = self.get_json(
+                usermap_url, headers={'Authorization': 'Bearer %s' % access_token})
+            data.update(usermap)
+        except ValueError:
+            pass
+        return data
